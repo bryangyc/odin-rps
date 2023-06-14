b18aa09abd9feb464574d7520dc72f97196e1a03
Bryan Goh <bryan.gohyc@gmail.com>
2023-06-14 17:55:43 +0800

    Element Listener

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 let signs = ['rock', 'paper', 'scissors'];
+let computerScore = 0;
+let playerScore = 0;
+let noOfGames = 0;
+let playerSelection;
+let computerSelection;
 
 function getComputerChoice() {
     let com = Math.floor(Math.random() * signs.length);
@@ -9,43 +14,98 @@ function getComputerChoice() {
 
 // function that plays a single round of the game
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
+    if (playerSelection === computerSelection) {
+        return "Tie"
+    }
+    if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')
+    ) {
         computerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "Computer Wins"
+    }
+    if (
+        (computerSelection === 'rock' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'rock')
+    ) {
         playerScore++;
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore++;
-    } else;
+        return "Player Wins"
+    }
     // console.log(playerScore, computerScore);
 }
 
 // let playerSelection = "rock".toLowerCase()
-let computerScore = 0;
-let playerScore = 0;
 // console.log(playerSelection, computerSelection)
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose a sign").toLocaleLowerCase();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(playerScore, computerScore);
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt("Choose a sign").toLocaleLowerCase();
+//         let computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         console.log(playerScore, computerScore);
+//     }
+// }
+
+// game()
+
+
+// function game() {
+//     const buttons = document.querySelectorAll('button');
+//     buttons.forEach(button => button.addEventListener('click', function (e) {
+// console.log(e.target.innerHTML);
+//         playerSelection = e.target.textContent;
+// console.log(playerSelection);
+//         computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         console.log(playerScore, computerScore, playerSelection, computerSelection, noOfGames);
+//     }))
+// }
+// game()
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', function (e) {
+    // console.log(e.target.innerHTML);
+    playerSelection = e.target.textContent;
+    // console.log(playerSelection);
+    computerSelection = getComputerChoice();
+    
+    // playRound(playerSelection, computerSelection);
+    if (playerSelection === computerSelection) {
+        console.log("Tie")
     }
-}
+    if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')
+        ) {
+            computerScore++;
+        }
+        if (
+            (computerSelection === 'rock' && playerSelection === 'paper') ||
+            (computerSelection === 'paper' && playerSelection === 'scissors') ||
+            (computerSelection === 'scissors' && playerSelection === 'rock')
+            ) {
+                playerScore++;
+            }
+            // console.log(playerSelection, computerSelection);
+        console.log(playerScore, computerScore, playerSelection, computerSelection);
+}))
+
+// playRound('rock','scissors');
+// Why isn't the variable reading outside of the function? 
+// playRound(playerSelection, computerSelection);
+// console.log(playerScore, computerScore, playerSelection, computerSelection, noOfGames);
+
+if (playerScore === 5) {
+    console.log("Player Wins");
+} else if (computerScore === 5) {
+    console.log("Computer Wins");
+} else;
+    // console.log(computerSelection);
+    // console.log(playerSelection);
 
 
-game()
-
-if (playerScore < computerScore) {
-    console.log('You Lose')
-} else if (computerScore < playerScore) {
-    console.log('You Win')
-} else console.log('Tie')
 
 // console.log(getComputerChoice())
